a050a80e5538251ba66695e9cb839da68e72b083
yaseno2186 <yaseno2186@users.noreply.github.com>
2024-07-25 10:42:31 +0000

    deploy: db7e982b9dc6ee093e6125e6e1afd8e67d7b4b5f

--- snapshot/target/generated-sources/antlr4/org/jqassistant/plugin/c4/antlr/C4Listener.java
+++ snapshot/target/generated-sources/antlr4/org/jqassistant/plugin/c4/antlr/C4Listener.java
@@ -1,4 +1,4 @@
-// Generated from org/jqassistant/plugin/c4/antlr/C4.g4 by ANTLR 4.7.2
+// Generated from org/jqassistant/plugin/c4/antlr/C4.g4 by ANTLR 4.13.1
 package org.jqassistant.plugin.c4.antlr;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 
